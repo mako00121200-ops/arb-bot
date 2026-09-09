df20879c42a36905397fca003f98af55fdc72053
mako00121200-ops <mako00121200@gmail.com>
2026-09-09 19:57:58 +0900

    Update Polygon RPC URL in chain-config.js

--- chain-config.js
+++ chain-config.js
@@ -14,7 +14,7 @@ export const CHAIN_CONFIG = {
     explorerTxUrl: (hash) => `https://basescan.org/tx/${hash}`,
   },
   polygon: {
-    rpcUrl: "https://polygon-rpc.com",
+    rpcUrl: "https://polygon.llamarpc.com",
     aavePoolAddressesProvider: AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
     contractAddressEnvVar: "MAINNET_CONTRACT_ADDRESS_POLYGON",
     explorerTxUrl: (hash) => `https://polygonscan.com/tx/${hash}`,
